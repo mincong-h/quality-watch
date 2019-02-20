698682304aaf68823ac3ff1daff4761283176109
Mincong HUANG <mincong.h@gmail.com>
2019-02-20 22:14:01 +0100

    Improve test coverage

--- src/test/java/qwatch/logs/util/JsonImportUtilTest.java
+++ src/test/java/qwatch/logs/util/JsonImportUtilTest.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -21,9 +22,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JsonImportUtilTest {
   @Rule public TemporaryFolder tempDir = new TemporaryFolder();
 
-  @Test
-  public void importLogEntriesFromFile_noDuplicates() throws Exception {
-    // Given an existing JSON file with duplicates
+  private Path json;
+
+  @Before
+  public void setUp() throws Exception {
+    json = tempDir.newFile("log.2019-02-14.json").toPath();
     String content =
         "[{\n"
             + "  \"date\" : \"2019-02-14T12:44:20.962Z\",\n"
@@ -38,9 +41,12 @@ public class JsonImportUtilTest {
             + "  \"status\" : \"error\",\n"
             + "  \"message\" : \"Project experimental2017 not found.\"\n"
             + "}]";
-    Path json = tempDir.newFile().toPath();
     Files.write(json, content.getBytes(UTF_8));
+  }
 
+  @Test
+  public void importLogEntriesFromFile_noDuplicates() {
+    // Given an existing JSON file with duplicates
     // When importing the content
     Set<LogEntry> logEntries = JsonImportUtil.importLogEntriesFromFile(json).get();
 
@@ -62,5 +68,11 @@ public class JsonImportUtilTest {
   public void importLogEntriesFromFile_notJson() {
     Path nonexistent = tempDir.getRoot().toPath().resolve("nonexistent");
     assertThat(JsonImportUtil.importLogEntriesFromFile(nonexistent).isFailure()).isTrue();
+  }
+
+  @Test
+  public void importLogEntries_noDuplicates() {
+    Set<LogEntry> logEntries = JsonImportUtil.importLogEntries(tempDir.getRoot().toPath()).get();
+    assertThat(logEntries).hasSize(1);
   }
 }
